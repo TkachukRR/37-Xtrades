03fe7fb127fc7f0488b0127ea386503a8d10ae8e
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-16 00:54:47 +0100

    Added ability get winner weekly challenges, double gain week contest, winner double gain week contest

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,12 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { TopBarComponent } from "../top-bar/top-bar.component";
 import { StatisticComponent } from "../satistic/statistic.component";
 import { Image } from "../../interfaces/image";
 import { UserService } from "../../services/user.service";
 import { UserStatistic } from "../../interfaces/user-statistic";
-import {Subscription} from "rxjs";
+import { Subscription } from "rxjs";
+import { Winner } from "../../interfaces/winner";
+import { Title } from "../../custom/statistic-title.enum";
 
 @Component({
   selector: 'app-main-page',
@@ -21,13 +23,22 @@ export class MainPageComponent implements OnInit, OnDestroy{
   private subs: Subscription[] = [];
   participants: Image[] = [];
   weeklyChallenges: UserStatistic[] = [];
+  winnerWeeklyChallenges: Winner = {} as Winner;
+  doubleGainWeekContest: UserStatistic[] = [];
+  winnerDoubleGainWeekContest: Winner = {} as Winner;
+
 
   ngOnInit(): void {
     this.subs.push(this._us.getParticipants().subscribe(participants => this.participants = participants));
     this.subs.push(this._us.getWeeklyChallenges().subscribe(statistic => this.weeklyChallenges = statistic));
+    this.subs.push(this._us.getWinnerWeeklyChallenges().subscribe(w => this.winnerWeeklyChallenges = w));
+    this.subs.push(this._us.getDoubleGainWeekContest().subscribe(statistic => this.doubleGainWeekContest = statistic));
+    this.subs.push(this._us.getWinnerDoubleGainWeekContest().subscribe(w => this.winnerDoubleGainWeekContest = w));
   }
 
   ngOnDestroy(): void {
     this.subs.forEach(sub => sub.unsubscribe())
   }
+
+  protected readonly Title = Title;
 }
