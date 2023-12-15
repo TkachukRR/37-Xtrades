1754d0d56a53f1b022055bf6d0889ecc2577f703
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 14:32:29 +0100

    Improved subscriptions

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import { TopBarComponent } from "../top-bar/top-bar.component";
 import { StatisticComponent } from "../satistic/statistic.component";
 import { Image } from "../../interfaces/image";
 import { UserService } from "../../services/user.service";
 import { UserStatistic } from "../../interfaces/user-statistic";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-main-page',
@@ -15,13 +16,18 @@ import { UserStatistic } from "../../interfaces/user-statistic";
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent implements OnInit{
+export class MainPageComponent implements OnInit, OnDestroy{
   private _us = inject(UserService);
+  private subs: Subscription[] = [];
   participants: Image[] = [];
   weeklyChallenges: UserStatistic[] = [];
 
   ngOnInit(): void {
-    this._us.getParticipants().subscribe(participants => this.participants = participants);
-    this._us.getWeeklyChallenges().subscribe(statistic => this.weeklyChallenges = statistic);
+    this.subs.push(this._us.getParticipants().subscribe(participants => this.participants = participants));
+    this.subs.push(this._us.getWeeklyChallenges().subscribe(statistic => this.weeklyChallenges = statistic));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe())
   }
 }
