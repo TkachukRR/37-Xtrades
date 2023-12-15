289942e4fa5ae4797211cbaf42b1ccd28fe1dc88
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 14:05:48 +0100

    Added basic structure and styles to statistic component

--- src/app/components/satistic/statistic.component.ts
+++ src/app/components/satistic/statistic.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { NgForOf, NgIf, TitleCasePipe, UpperCasePipe } from "@angular/common";
 import { Image } from "../../interfaces/image";
 
 @Component({
   selector: 'app-statistic',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleCasePipe,
+    UpperCasePipe,
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './statistic.component.html',
   styleUrl: './statistic.component.scss'
 })
 export class StatisticComponent {
   @Input() title = ''
   @Input() participants: Image[] = []
- }
+}
