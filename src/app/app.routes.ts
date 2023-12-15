462ce24edfd87d16d819229e58f97e037b5bb31d
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 10:03:57 +0100

    Registered error page component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from "./components/main-page/main-page.component";
+import { ErrorPageComponent } from "./components/error-page/error-page.component";
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: 'error', component: ErrorPageComponent},
   { path: '**', redirectTo: '' }
 ];
