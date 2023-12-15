c3642f534587c4d0e503e56123346cd336f54585
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 11:03:11 +0100

    Generated statistic component

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { TopBarComponent } from "../top-bar/top-bar.component";
+import { SatisticComponent } from "../satistic/satistic.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    TopBarComponent
+    TopBarComponent,
+    SatisticComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
---
+++ src/app/components/satistic/satistic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-satistic',
+  standalone: true,
+  imports: [],
+  templateUrl: './satistic.component.html',
+  styleUrl: './satistic.component.scss'
+})
+export class SatisticComponent {
+
+}
