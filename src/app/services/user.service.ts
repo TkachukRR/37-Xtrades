9c61c29dd4a4ec8bfd1009ab50173f5aae751ea6
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 09:46:33 +0100

    Implemented User service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,39 @@
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, delay, Observable } from "rxjs";
+import { User } from "../interfaces/user";
+import { HttpClient } from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _http = inject(HttpClient);
+  private baseMockedDataUrl = 'assets/mock-data/'; //TODO Change to real API
+
+  private userSubj = new BehaviorSubject({} as User);
+  private user$ = this.userSubj.asObservable();
+
+  private loadingUserDataSubj = new BehaviorSubject(true);
+  private loadingUserData$ = this.loadingUserDataSubj.asObservable()
+  constructor() {
+    this.fetchUserInfo()
+  }
+
+  fetchUserInfo(): void{
+    if (!this.loadingUserDataSubj.value) this.loadingUserDataSubj.next(true)
+
+    this._http.get<User>( this.baseMockedDataUrl + 'user.json').pipe(
+      delay(2000) //TODO emulation server delay (delete)
+    ).subscribe(user => {
+      this.loadingUserDataSubj.next(false)
+      this.userSubj.next(user)
+    })
+  }
+
+  getUserInfo(): Observable<User>{
+    return this.user$
+  }
+
+  getLoadingUserDataStatus(): Observable<boolean> {
+    return this.loadingUserData$
+  }
+}
