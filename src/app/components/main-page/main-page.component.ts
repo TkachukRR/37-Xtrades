09b93e373d51b3f7c9d8d411505c9b708ed21a65
Roman Tkachuk <tkachukrr@gmail.com>
2023-12-15 14:29:54 +0100

    Implemented ability get weekly changes in main page component

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -3,6 +3,7 @@ import { TopBarComponent } from "../top-bar/top-bar.component";
 import { StatisticComponent } from "../satistic/statistic.component";
 import { Image } from "../../interfaces/image";
 import { UserService } from "../../services/user.service";
+import { UserStatistic } from "../../interfaces/user-statistic";
 
 @Component({
   selector: 'app-main-page',
@@ -17,8 +18,10 @@ import { UserService } from "../../services/user.service";
 export class MainPageComponent implements OnInit{
   private _us = inject(UserService);
   participants: Image[] = [];
+  weeklyChallenges: UserStatistic[] = [];
 
   ngOnInit(): void {
-    this._us.getParticipants().subscribe(participants => this.participants = participants)
+    this._us.getParticipants().subscribe(participants => this.participants = participants);
+    this._us.getWeeklyChallenges().subscribe(statistic => this.weeklyChallenges = statistic);
   }
 }
